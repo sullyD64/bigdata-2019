11f93d8cb8813a101e78092b53bc559ea4dec17f
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-05-27 23:48:45 +0200

    mapreduce/job3, added decode string for previously encoded trend (p/n)

--- project1/src/mapreduce/job3/thirdReducer.py
+++ project1/src/mapreduce/job3/thirdReducer.py
@@ -1,58 +1,81 @@
 from itertools import combinations
 import sys
+import re
 
 # COMPANY SECTOR YEAR:GROWTH
 # 0       1      2
 
+
+# decodes trend string
+def pretty_format(trend_str):
+    if trend_str:
+        trend = re.sub("[\s\[\]']", "", trend_str).split(",")
+        out_trend = []
+        for year_growth in trend:
+            year, growth = year_growth.split(':')
+            sign = growth[-1:]
+            if sign == 'p':
+                growth = "+" + growth[:1] + "%"
+            elif sign == 'n':
+                growth = "-" + growth[:1] + "%"
+            out_trend.append(year + ':' + growth)
+        print(out_trend)
+        return out_trend
+
+
 class Company:
-  def __init__(self, name=None, sector=None):
-    self.name = name
-    self.sector = sector
-    self.trend = []
+    def __init__(self, name=None, sector=None):
+        self.name = name
+        self.sector = sector
+        self.trend = []
 
-  def update(self, year_growth):
-    self.trend.append(year_growth)
+    def update(self, year_growth):
+        self.trend.append(year_growth)
 
-  def equals(self, that):
-    return (self.name == that.name and self.sector == that.sector)
+    def equals(self, that):
+        return (self.name == that.name and self.sector == that.sector)
 
-  def __repr__(self):
-    return str([self.name, self.sector])
-  
-    
+    def __repr__(self):
+        return str([self.name, self.sector])
+
+
 class SimilarTrendingCompanies:
-  def __init__(self, trend=None):
-    self.trend = trend
-    self.companies = []
+    def __init__(self, trend=None):
+        self.trend = pretty_format(trend)
+        self.companies = []
 
-  def update(self, company):
-    self.companies.append(company)
+    def update(self, company):
+        self.companies.append(company)
 
-  def generate_similar_couples(self):
-    for couple in combinations(self.companies, r=2): 
-      if not(couple[0].sector == couple[1].sector):
-        print('%s\t%s\t%s' % (self.trend, couple[0].name, couple[1].name))
+    def generate_similar_couples(self):
+        for couple in combinations(self.companies, r=2):
+            if not(couple[0].sector == couple[1].sector):
+                print('%s\t%s\t%s' %
+                      (self.trend,
+                       ':'.join([couple[0].name, couple[0].sector]),
+                       ':'.join([couple[1].name, couple[1].sector])))
 
 
 def main(input_file):
-  curr_trend = None
-  curr_trend_companies = SimilarTrendingCompanies()
+    curr_trend = None
+    curr_trend_companies = SimilarTrendingCompanies()
 
-  for line in input_file:
-    trend, name, sector = line.strip().split('\t')
-  
-    if curr_trend != trend:
-      if curr_trend:
-        curr_trend_companies.generate_similar_couples()
-      curr_trend = trend
-      curr_trend_companies = SimilarTrendingCompanies(curr_trend)
+    for line in input_file:
+        trend, name, sector = line.strip().split('\t')
 
-    curr_trend_companies.update(Company(name, sector))
-    # print('%s\t%s' % (line.strip() , len(curr_trend_companies.companies)))  # for debug purposes
+        if curr_trend != trend:
+            if curr_trend:
+                curr_trend_companies.generate_similar_couples()
+            curr_trend = trend
+            curr_trend_companies = SimilarTrendingCompanies(curr_trend)
 
-  # print last company annual report
-  curr_trend_companies.generate_similar_couples()
+        curr_trend_companies.update(Company(name, sector))
+        # for debug purposes
+        print('%s\t%s' % (line.strip(), len(curr_trend_companies.companies)))
+
+    # print last trend couples
+    curr_trend_companies.generate_similar_couples()
+
 
 if __name__ == "__main__":
     main(sys.stdin)
-
