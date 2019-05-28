621bb2a5c91324a803362df66713d6441824b438
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-05-29 01:38:16 +0200

    Small namespace fixes

--- project1/src/mapreduce/job2/firstMapper_join.py
+++ project1/src/mapreduce/job2/firstMapper_join.py
@@ -1,14 +1,5 @@
 import sys
-import re
 import csv
-
-# historical_stock_prices.csv (8 fields):
-# (TICKER, OPEN, CLOSE, ADJ_CLOSE, LOW, HIGH, VOLUME, DATE)
-#  0       1     2      3          4    5     6       7
-
-# historical_stocks.csv (5 fields):
-# (TICKER, EXCHANGE, NAME, SECTOR, INDUSTRY)
-# 0        1         2     3       4
 
 MIN_DATE = "2004-01-01"
 MAX_DATE = "2018-12-31"
