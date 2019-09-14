5e29bc75f0c6fded9a0eae26808c1e7381243e63
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-09-14 19:12:40 +0200

    small fixes again

--- project2/s0_prefilter_shorten_urls.py
+++ project2/s0_prefilter_shorten_urls.py
@@ -26,13 +26,14 @@ SKIP_PATTERNS = "@(?!en)|"\
 
 
 def run_job(rdd):
-    rdd = rdd
-        .filter(lambda x: not re.findall(SKIP_PATTERNS, x)
-        .map(lambda x: x.replace(FB_PREDIX, NEW_PREFIX)
+    rdd = rdd \
+        .filter(lambda x: not re.findall(SKIP_PATTERNS, x) \
+        .map(lambda x: x.replace(FB_PREDIX, NEW_PREFIX) \
+        .collect()
 
     # TODO implement save new rdd
-    for l in rdd.collect():
-        print(l)  
+    for l in rdd:
+        print(l)
 
 
 if __name__ == "__main__":
