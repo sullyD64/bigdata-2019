e9ee992c4e1c547a18b6df6d06d4cea032e0b5dd
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2019-09-15 16:55:47 +0200

    Add prefix shortening for the /key/ namespace.

--- project2/s0_prefilter_shorten_urls.py
+++ project2/s0_prefilter_shorten_urls.py
@@ -9,8 +9,11 @@ PROT = "file://"
 INPUT = "/home/freebase/fb1m"
 OUTPUT = INPUT + "-s0"
 
-URI_PREFIX = "http://rdf.freebase.com/ns/"
-NEW_URI_PREFIX = "f:"
+NS_PREFIX = "http://rdf.freebase.com/ns/"
+NEW_NS_PREFIX = "f:"
+
+KEY_PREFIX = "http://rdf.freebase.com/key"
+NEW_KEY_PREFIX = "k:"
 
 # TODO refine cleaning strategy
 SKIP_PATTERNS = "@(?!en)|"\
@@ -25,14 +28,14 @@ SKIP_PATTERNS = "@(?!en)|"\
     + "/common\.document\.(?!source_uri)\b.*|"\
     + "/common\.topic\.(description|image|webpage|properties|weblink|notable_for|article).*|"\
     + "/dataworld\.|"\
-    + "/key/.*|"\
     + "/base\."
-
+    
 
 def run_job(rdd):
     rdd = rdd \
         .filter(lambda x: not re.findall(SKIP_PATTERNS, x)) \
-        .map(lambda x: x.replace(URI_PREFIX, NEW_URI_PREFIX))
+        .map(lambda x: x.replace(NS_PREFIX, NEW_NS_PREFIX)
+                        .replace(KEY_PREFIX, NEW_KEY_PREFIX))
 
     rdd.repartition(1).saveAsTextFile(PROT + OUTPUT)
     # for l in rdd.collect():
